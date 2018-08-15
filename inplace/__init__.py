929390c55be13a78352c086d12c9e500e3e96cb1
Danilo Horta <danilo.horta@gmail.com>
2018-08-16 00:13:13 +0100

    working

--- inplace/__init__.py
+++ inplace/__init__.py
@@ -2,4 +2,4 @@ from __future__ import absolute_import
 
 __version__ = "0.0.1"
 
-__all__["__version__"]
+__all__ = ["__version__"]
--- inplace/inplace.py
+++ inplace/inplace.py
@@ -1,9 +1,9 @@
-# from __future__ import unicode_literals
-import sys
-from PyInquirer import prompt, Separator
-from subprocess import check_output, call
-from tqdm import tqdm
+from __future__ import unicode_literals
+import os
+from PyInquirer import prompt, Separator, style_from_dict, Token
+from subprocess import check_output, call, CalledProcessError
 import click
+from inplace import __version__
 
 question = {
     "type": "checkbox",
@@ -14,28 +14,45 @@ question = {
 
 
 @click.command()
-@click.argument("dir", nargs=1)
+@click.argument("path", nargs=1)
 @click.argument("regex", nargs=1)
-@click.version_option(version="0.0.1")
-def entry(dir, regex):
-    files = check_output(["find", dir, "-type", "f", "-print0"])
+@click.version_option(version=__version__)
+def entry(path, regex):
+    """Find and replace files interactively.
+
+    """
+    if not os.path.exists(path):
+        click.echo("{} does not exist.".format(path))
+        return 1
+
+    click.echo("Compiling the file list... ", nl=False)
+    files = check_output(["find", path, "-type", "f", "-print0"])
+    click.echo("done.")
 
     files = files.split(b"\0")[:-1]
     matched_files = []
     max_matches = -1
-    for f in tqdm(files, desc="Searching"):
+    click.echo("Finding matches... ", nl=False)
+    for f in files:
         f = f.decode()
 
         arg = 'my $number = $_ =~ {}; print scalar "$number "'.format(regex)
-        o = check_output(["perl", "-ne", arg, f])
+        try:
+            o = check_output(["perl", "-ne", arg, f])
+        except CalledProcessError as e:
+            return e.returncode
+
         o = b" ".join(o.strip().split())
         if len(o) > 0:
             n = sum(int(i) for i in o)
             matched_files.append((f, n))
             max_matches = max(max_matches, n)
 
+    click.echo("done.")
+
     if max_matches < 0:
-        return
+        click.echo("No match has been found.")
+        return 0
 
     num_siz = len(str(max_matches))
     left_siz = num_siz + len(" match(es): ")
@@ -46,22 +63,23 @@ def entry(dir, regex):
 
     answers = prompt(question)
     if "files" not in answers:
+        # click.echo("Completed successfully.")
         return 0
 
     nfails = 0
     for a in answers["files"]:
         filepath = a[left_siz:]
-        sys.stdout.write(filepath + "... ")
+        click.echo(filepath + "... ", nl=False)
         e = call(["perl", "-p", "-i", "-e", regex, filepath])
         if e == 0:
-            print("done.")
+            click.echo("done.")
         else:
-            print("FAILED!")
+            click.echo("FAILED!")
             nfails += 1
 
     if nfails == 0:
-        print("Completed successfully.")
+        click.echo("Completed successfully.")
         return 0
     else:
-        print("Failed to change {} files!".format(nfails))
+        click.echo("Failed to change {} files!".format(nfails))
         return 1
